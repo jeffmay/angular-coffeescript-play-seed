5208b5fad21884a1614de1893560959a2f88a780
Jeff May <jeff.may@audaxhealth.com>
2013-06-16 20:19:24 -0400

    Fixed application spec.

--- test/controllers/ApplicationSpec.scala
+++ test/controllers/ApplicationSpec.scala
@@ -7,19 +7,17 @@ import play.api.test.Helpers._
 
 class ApplicationSpec extends Specification {
   
-
-  /*
   "Application" should {
     
     "send 404 on a bad request" in {
       running(FakeApplication()) {
-        routeAndCall(FakeRequest(GET, "/boum")) must beNone        
+        route(FakeRequest(GET, "/boum")) must beNone
       }
     }
     
     "render an empty form on index" in {
       running(FakeApplication()) {
-        val home = routeAndCall(FakeRequest(GET, "/")).get
+        val home = route(FakeRequest(GET, "/")).get
         
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
@@ -28,7 +26,7 @@ class ApplicationSpec extends Specification {
     
     "send BadRequest on form error" in {
       running(FakeApplication()) {
-        val home = routeAndCall(FakeRequest(GET, "/hello?name=Bob&repeat=xx")).get
+        val home = route(FakeRequest(GET, "/hello?name=Bob&repeat=xx")).get
         
         status(home) must equalTo(BAD_REQUEST)
         contentType(home) must beSome.which(_ == "text/html")
@@ -37,7 +35,7 @@ class ApplicationSpec extends Specification {
     
     "say hello" in {
       running(FakeApplication()) {
-        val home = routeAndCall(FakeRequest(GET, "/hello?name=Bob&repeat=10")).get
+        val home = route(FakeRequest(GET, "/hello?name=Bob&repeat=10")).get
         
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
@@ -45,6 +43,5 @@ class ApplicationSpec extends Specification {
     }
     
   }
-  */
-  
+
 }
